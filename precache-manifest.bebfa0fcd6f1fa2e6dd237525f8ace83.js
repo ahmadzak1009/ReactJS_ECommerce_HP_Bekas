10597c489639f428e69e6962fb4825b9ccaca21f
ahmadzak1009 <ahmadzak1009@bsi.ac.id>
2019-11-25 22:36:05 +0700

    Updates

---
+++ precache-manifest.bebfa0fcd6f1fa2e6dd237525f8ace83.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe816dae9e800b097bfb7764e84e9f9",
+    "url": "/ReactJS_ECommerce_HP_Bekas/index.html"
+  },
+  {
+    "revision": "ef9673c9ec347cb38c94",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/css/main.9c5a1d25.chunk.css"
+  },
+  {
+    "revision": "649f050d39db6cd0a629",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/js/2.96627088.chunk.js"
+  },
+  {
+    "revision": "ef9673c9ec347cb38c94",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/js/main.d2509b55.chunk.js"
+  },
+  {
+    "revision": "8e00fdc62994168f3d18",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/js/runtime-main.c91b7291.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/ReactJS_ECommerce_HP_Bekas/static/media/product-8.d390282f.png"
+  }
+]);
